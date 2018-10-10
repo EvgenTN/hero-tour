654acd679006d17b735bacb8b0b176b13522f63b
evgentn <evgentitovn@gmail.com>
2018-10-10 20:31:13 +0300

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[]
+
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  ngOnInit() {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(
+        (heroes: Hero[]) => {
+          this.heroes = heroes.slice(0, 4);
+        }
+      );
+  }
+
+}
